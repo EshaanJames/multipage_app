a9c617f063ef55d6e38b37b61eb4d318df6a4ae5
EshaanJames <77269765+EshaanJames@users.noreply.github.com>
2021-07-30 19:24:07 +0530

    Add files via upload

---
+++ plots.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+import seaborn as sns
+
+def app(car_df):
+	st.header("Vidualise Data")
+	st.set_option("deprecation.showPyplotGlobalUse", False)
+	st.subheader("Scatter Plot")
+	features_list = st.multiselect("Select the X-axis values.", ('carwidth', 'enginesize', 'horsepower', 'drivewheel_fwd', 'car_company_buick'))
+	for i in features_list:
+		st.subheader(f"Scatter plot between {i} and price.")
+		plt.figure(figsize = (15, 8))
+		sns.scatterplot(x = i, y = 'price', data = car_df)
+		st.pyplot()
+	st.subheader("Visualisation selector")
+	plot_type = st.multiselect("Select Plots to be shown.", ("Histogram", "BoxPlot", "Correlation Heatmap"))
+	if "Histogram" in plot_type:
+		st.subheader("Histogram")
+		columns = st.selectbox("Select the column to create the Histogram",('carwidth', 'enginesize', 'horsepower'))
+		plt.figure(figsize = (12,6))
+		plt.title(f"Historam for {columns}")
+
+		plt.hist(car_df[columns], bins = 'sturges', edgecolor = 'black')
+		st.pyplot()
+	if "BoxPlot" in plot_type:
+		st.subheader("Boxplot")
+		columns = st.selectbox("Select the column to create the Boxplot",('carwidth', 'enginesize', 'horsepower'))
+		plt.figure(figsize = (12,6))
+		plt.title(f"BoxPlot for {columns}")
+
+		sns.boxplot( car_df[columns])
+		st.pyplot()
+	if "Correlation Heatmap" in plot_type:
+		st.subheader("Correlation Heatmap")
+		columns = st.selectbox("Select the column to create the Correlation Heatmap",('carwidth', 'enginesize', 'horsepower'))
+		plt.figure(figsize = (12,6))
+		plt.title(f"Correlation Heatmap for {columns}")
+		sns.heatmap(car_df[columns].corr(), annote = True)
+		st.pyplot()
+		
+		
---
+++ predict.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score,mean_squared_log_error
+
+@st.cache
+def prediction(car_df, carwidth, enginesize, hp, drivewheel_fwd, cc_buick ):
+	X = car_df.iloc[ : , : -1]
+	y = car_df['price']
+
+	X_train, X_test, y_train, y_test  = train_test_split(X, y, random_state = 42, test_size = 0.3)
+	
+	lin_reg = LinearRegression()
+	lin_reg.fit(X_train, y_train)
+	mod_score = lin_reg.score(X_train, y_train)
+	
+	price = lin_reg.predict([[carwidth, enginesize, hp, drivewheel_fwd, cc_buick]])
+	price = price[0]
+
+	ytest_predict = lin_reg.predict(X_test)
+	test_r2 = r2_score(y_test, ytest_predict)
+	test_mae = mean_absolute_error(y_test, ytest_predict)
+	test_rmse = np.sqrt(mean_squared_error(y_test, ytest_predict))
+	test_msle = mean_squared_log_error(y_test, ytest_predict)
+	return price, mod_score, test_r2, test_mae, test_rmse, test_msle
+
+def app(car_df):
+	st.markdown("<p style = 'color:red; font-size:25px'> This app uses <b>LinearRegression</b> to predict the price of the car using the features.", unsafe_allow_html = True)
+	st.subheader('Select values')
+	
+	cw  = st.slider("Car Width", float(car_df['carwidth'].min()), float(car_df['carwidth'].max()))
+	esize  = st.slider("Engine Size", float(car_df['enginesize'].min()), float(car_df['enginesize'].max()))
+	hpower = st.slider("Horse Power", float(car_df['horsepower'].min()), float(car_df['horsepower'].max()))
+	d_fwd = st.radio("Is it a forward driving wheel car?", ('Yes', 'No'))
+	
+	if d_fwd == 'No':
+		d_fwd =0
+	else:
+		d_fwd = 1
+	ccb = st.radio("Is the car manufactured by buick?", ('Yes', 'No'))
+	if ccb == 'No':
+		ccb = 0
+	else:
+		ccb  = 1
+		
