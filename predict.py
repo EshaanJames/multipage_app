2fe6ed85b867de052af59cd40a0781355080e870
EshaanJames <77269765+EshaanJames@users.noreply.github.com>
2021-07-30 19:43:00 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,58 +1,61 @@
-import streamlit as st
+# Code for 'predict.py' file.
+
+# Importing the necessary Python modules.
 import numpy as np
 import pandas as pd
+import streamlit as st
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score,mean_squared_log_error
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error, mean_squared_log_error
 
-@st.cache
-def prediction(car_df, carwidth, enginesize, hp, drivewheel_fwd, cc_buick ):
-	X = car_df.iloc[ : , : -1]
-	y = car_df['price']
+@st.cache()
+def prediction(car_df, car_width, engine_size, horse_power, drive_wheel_fwd, car_comp_buick):
+    X = car_df.iloc[:, :-1] 
+    y = car_df['price']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 42) 
 
-	X_train, X_test, y_train, y_test  = train_test_split(X, y, random_state = 42, test_size = 0.3)
-	
-	lin_reg = LinearRegression()
-	lin_reg.fit(X_train, y_train)
-	mod_score = lin_reg.score(X_train, y_train)
-	
-	price = lin_reg.predict([[carwidth, enginesize, hp, drivewheel_fwd, cc_buick]])
-	price = price[0]
+    lin_reg = LinearRegression()
+    lin_reg.fit(X_train, y_train)
+    score = lin_reg.score(X_train, y_train)
 
-	ytest_predict = lin_reg.predict(X_test)
-	test_r2 = r2_score(y_test, ytest_predict)
-	test_mae = mean_absolute_error(y_test, ytest_predict)
-	test_rmse = np.sqrt(mean_squared_error(y_test, ytest_predict))
-	test_msle = mean_squared_log_error(y_test, ytest_predict)
-	return price, mod_score, test_r2, test_mae, test_rmse, test_msle
+    price = lin_reg.predict([[car_width, engine_size, horse_power, drive_wheel_fwd, car_comp_buick]])
+    price = price[0]
 
-def app(car_df):
-	st.markdown("<p style = 'color:red; font-size:25px'> This app uses <b>LinearRegression</b> to predict the price of the car using the features.", unsafe_allow_html = True)
-	st.subheader('Select values')
-	
-	cw  = st.slider("Car Width", float(car_df['carwidth'].min()), float(car_df['carwidth'].max()))
-	esize  = st.slider("Engine Size", float(car_df['enginesize'].min()), float(car_df['enginesize'].max()))
-	hpower = st.slider("Horse Power", float(car_df['horsepower'].min()), float(car_df['horsepower'].max()))
-	d_fwd = st.radio("Is it a forward driving wheel car?", ('Yes', 'No'))
-	
-	if d_fwd == 'No':
-		d_fwd =0
-	else:
-		d_fwd = 1
-	ccb = st.radio("Is the car manufactured by buick?", ('Yes', 'No'))
-	if ccb == 'No':
-		ccb = 0
-	else:
-		ccb  = 1
-	
-	if st.button("Predict"):
-		st.subheader("Prediction results.")
-		price, mod_score, test_r2, test_mae, test_rmse, test_msle =  prediction(car_df, cw, esize, hpower, d_fwd, ccb)
-		st.success(f"Predicted Price of the car is ${int(price)}.")
-		st.info(f"""
-			Accuracy score of the model is {mod_score:.2f}.
-			Squared error = {test_r2:.2f}.
-			Mean absolute error = {test_mae:.2f}
-			Root MEan squared error = {test_rmse:.2f}
-			Mean Squared log error = {test_msle:.2f}
-			""")
+    y_test_pred = lin_reg.predict(X_test)
+    test_r2_score = r2_score(y_test, y_test_pred)
+    test_mae = mean_absolute_error(y_test, y_test_pred)
+    test_msle = mean_squared_log_error(y_test, y_test_pred)
+    test_rmse = np.sqrt(mean_squared_error(y_test, y_test_pred))
+
+    return price, score, test_r2_score, test_mae, test_msle, test_rmse
+
+# Defining a function 'app()' which accepts 'car_df' as an input.
+def app(car_df): 
+    st.markdown("<p style='color:blue;font-size:25px'>This app uses <b>Linear regression</b> to predict the price of a car based on your inputs.", unsafe_allow_html = True) 
+    st.subheader("Select Values:")
+    car_wid = st.slider("Car Width", float(car_df["carwidth"].min()), float(car_df["carwidth"].max()))     
+    eng_siz = st.slider("Engine Size", int(car_df["enginesize"].min()), int(car_df["enginesize"].max()))
+    hor_pow = st.slider("Horse Power", int(car_df["horsepower"].min()), int(car_df["horsepower"].max()))    
+    drw_fwd = st.radio("Is it a forward drive wheel car?", ("Yes", "No"))
+    if drw_fwd == 'No':
+        drw_fwd = 0
+    else:
+        drw_fwd = 1
+    com_bui = st.radio("Is the car manufactured by Buick?", ("Yes", "No"))
+    if com_bui == 'No':
+        com_bui = 0
+    else:
+        com_bui = 1
+    
+    # When 'Predict' button is clicked, the 'prediction()' function must be called 
+    # and the value returned by it must be stored in a variable, say 'price'. 
+    # Print the value of 'price' and 'score' variable using the 'st.success()' and 'st.info()' functions respectively.
+    if st.button("Predict"):
+        st.subheader("Prediction results:")
+        price, score, car_r2, car_mae, car_msle, car_rmse = prediction(car_df, car_wid, eng_siz, hor_pow, drw_fwd, com_bui)
+        st.success("The predicted price of the car: ${:,}".format(int(price)))
+        st.info("Accuracy score of this model is: {:2.2%}".format(score))
+        st.info(f"R-squared score of this model is: {car_r2:.3f}")  
+        st.info(f"Mean absolute error of this model is: {car_mae:.3f}")  
+        st.info(f"Mean squared log error of this model is: {car_msle:.3f}")  
+        st.info(f"Root mean squared error of this model is: {car_rmse:.3f}")
